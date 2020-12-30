2d726bbb2e8718cf35cd44799651a9d23c141e19
Bouchra Kostet <bouchra.kostet@gmail.com>
2020-12-30 14:37:24 +0400

    Seed code written

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_143036) do
+ActiveRecord::Schema.define(version: 2020_12_30_081020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2020_12_29_143036) do
     t.string "first_name"
     t.string "last_name"
     t.string "country"
+    t.string "date_of_birth"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,9 @@
 
 puts "Cleaning the DB"
 Question.delete_all
+Possible_answer.delete_all
+
+puts "DB sweeped clean"
 
 puts "Seeding Questions"
 
@@ -28,20 +31,29 @@ questions = [
   },
 
   {
-    question_detail: 'Which protection do you prefer?',
+    question_detail: 'Which menstrual protection do you prefer to use?',
     question_tag: 'protection',
   },
 
   {
-    question_detail: 'Which menstrual troubles do you experience?',
+    question_detail: 'Which troubles related to your cycle do you experience?',
     question_tag: 'trouble',
   },
 
   {
-    question_detail: 'Have you been diagnosed with a menstrual condition?',
+    question_detail: 'Have you been diagnosed with a gynecological condition?',
     question_tag: 'condition',
   },
 ]
+
+questions.each do |question|
+  new_question = Question.new(
+    question_detail: question[:question_tag],
+    question_tag: question[:question_tag]
+  )
+  new_question.save!
+  puts "New question about #{new_question.question_tag} created."
+end
 
 # Create the questions with Question.new and save in the ddb
 
@@ -49,34 +61,219 @@ puts "Seeding Possible Answers"
 
 possible_answers = [
   {
-    possible_answer_detail: 'Yes',
-    questions_id: Question.where(question_tag: 'hormone').id,
+    possible_answer_detail: 'Yes, and I am happy with it',
+    question_id: Question.where(question_tag: 'hormone').id,
   },
 
   {
-    possible_answer_detail: 'No',
-    questions_id: Question.where(question_tag: 'hormone').id,
+    possible_answer_detail: 'Yes, but I want to stop',
+    question_id: Question.where(question_tag: 'hormone').id,
   },
 
-  # {
-  #   question_details: 'How often do you get your periods?',
-  # },
+  {
+    possible_answer_detail: 'Not for the moment',
+    question_id: Question.where(question_tag: 'hormone').id,
+  },
 
-  # {
-  #   question_details: 'How would you describe your flow?',
-  # },
+  {
+    possible_answer_detail: 'No, never again',
+    question_id: Question.where(question_tag: 'hormone').id,
+  },
 
-  # {
-  #   question_details: 'Which protection do you prefer?',
-  # },
+  {
+    possible_answer_detail: 'No, never',
 
-  # {
-  #   question_details: 'Which menstrual troubles do you experience?',
-  # },
+    question_id: Question.where(question_tag: 'hormone').id,
+  },
 
-  # {
-  #   question_details: 'Have you been diagnosed with a menstrual condition?',
-  # },
+  {
+    possible_answer_detail: 'More than 28 days',
+    question_id: Question.where(question_tag: 'period').id,
+  },
+
+  {
+    possible_answer_detail: '21-28 days',
+    question_id: Question.where(question_tag: 'period').id,
+  },
+
+  {
+    possible_answer_detail: 'Less than 21 days',
+    question_id: Question.where(question_tag: 'period').id,
+  },
+
+  {
+    possible_answer_detail: 'Irregular',
+    question_id: Question.where(question_tag: 'period').id,
+  },
+
+  {
+    possible_answer_detail: 'No periods',
+    question_id: Question.where(question_tag: 'period').id,
+  },
+
+  {
+    possible_answer_detail: 'Light',
+    question_id: Question.where(question_tag: 'flow').id,
+  },
+
+  {
+    possible_answer_detail: 'Normal',
+    question_id: Question.where(question_tag: 'flow').id,
+  },
+
+  {
+    possible_answer_detail: 'Heavy',
+    question_id: Question.where(question_tag: 'flow').id,
+  },
+
+  {
+    possible_answer_detail: 'Irregular',
+    question_id: Question.where(question_tag: 'flow').id,
+  },
+
+  {
+    possible_answer_detail: 'Menstrual pads',
+    question_id: Question.where(question_tag: 'protection').id,
+  },
+
+  {
+    possible_answer_detail: 'Tampons',
+    question_id: Question.where(question_tag: 'protection').id,
+  },
+
+  {
+    possible_answer_detail: 'Menstrual cup',
+    question_id: Question.where(question_tag: 'protection').id,
+  },
+
+  {
+    possible_answer_detail: 'Absorbing underwear',
+    question_id: Question.where(question_tag: 'protection').id,
+  },
+
+  {
+    possible_answer_detail: 'Reusable pads',
+    question_id: Question.where(question_tag: 'protection').id,
+  },
+
+  {
+    possible_answer_detail: 'Others',
+    question_id: Question.where(question_tag: 'protection').id,
+  },
+
+  {
+    possible_answer_detail: 'Cramps',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Abdominal pain',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Bloating',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Inflammation',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Digestive disorders',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Pelvic & lombar pain',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Mood disorders',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Skin disorders',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Fatigue',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Irregular bleeding',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Urinary disorders',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Dyspaneuria',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Infertility',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Hot flushes',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Other',
+    question_id: Question.where(question_tag: 'trouble').id,
+  },
+
+  {
+    possible_answer_detail: 'Endometriosis',
+    question_id: Question.where(question_tag: 'condition').id,
+  },
+
+  {
+    possible_answer_detail: 'Policyctic Orarian Sydrom',
+    question_id: Question.where(question_tag: 'condition').id,
+  },
+
+  {
+    possible_answer_detail: 'Adenomyosis',
+    question_id: Question.where(question_tag: 'condition').id,
+  },
+
+  {
+    possible_answer_detail: 'Pelvic Inflamamatory Disease',
+    question_id: Question.where(question_tag: 'condition').id,
+  },
+
+  {
+    possible_answer_detail: 'No, but I suspect it',
+    question_id: Question.where(question_tag: 'condition').id,
+  },
+
+  {
+    possible_answer_detail: 'None',
+    question_id: Question.where(question_tag: 'condition').id,
+  },
 ]
 
 # Create the possible answers with Possibele_answer.new and save in the ddb
+
+possible_answers.each do |possible_answer|
+  new_possible_answer = Possible_answer.new(
+    possible_answer_detail: question[:possible_answer_detail],
+    question_tag: question[:question_tag]
+  )
+  new_possible_answer.save!
+  puts "New answers created."
+end
